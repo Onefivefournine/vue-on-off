d3159f8e31c29de373bad7d60457963876c6827c
Konstantin <onefivefournine@gmail.com>
2017-11-27 15:44:07 +0300

    scss->css

--- index.vue
+++ index.vue
@@ -1,6 +1,6 @@
-<style lang="scss">
-//@extend-elements
-%inner-before-after {
+<style scoped>
+.onoffswitch-inner:before,
+.onoffswitch-inner:after {
   display: block;
   float: left;
   width: 50%;
@@ -11,81 +11,87 @@
   height: 22px;
 }
 
-@mixin transition($args...) {
-  -webkit-transition: $args;
-  transition: $args;
-}
-
-$gray:#a3a6b2;
-$green:#6ae2a2;
-$lgray:darken(#f1f2f6, 5%);
 .onoffswitch {
   position: relative;
   width: 50px;
   -webkit-user-select: none;
   -moz-user-select: none;
   -ms-user-select: none;
-  &-checkbox {
-    display: none;
-    +.onoffswitch-label {
-      border: 1px solid $lgray;
-      @include transition(border 0.3s ease-in-out);
-    }
-    &:checked+.onoffswitch-label {
-      border: 1px solid $green;
-      @include transition(border 0.3s ease-in-out);
-      .onoffswitch-inner {
-        margin-left: 0;
-      }
-      .onoffswitch-switch {
-        right: 2px;
-      }
-    }
-    &[disabled]+.onoffswitch-label {
-      border: 1px solid $lgray;
-      cursor: not-allowed;
-      .onoffswitch-inner:before {
-        background-color: $gray;
-      }
-    }
-  }
-  &-label {
-    display: block;
-    overflow: hidden;
-    cursor: pointer;
-    margin-bottom: 0;
-    border: 1px solid $green;
-    border-radius: 100px;
-  }
-  &-inner {
-    display: block;
-    width: 200%;
-    margin-left: -100%;
-    @include transition(all 0.3s ease-in-out);
-    &:before {
-      @extend %inner-before-after;
-      content: " ";
-      background-color: $green;
-    }
-    &:after {
-      @extend %inner-before-after;
-      content: " ";
-      background-color: $lgray;
-    }
-  }
-  &-switch {
-    display: block;
-    width: 20px;
-    height: 20px;
-    margin: 2px 0 0;
-    background: #fff;
-    border-radius: 100%;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    right: 28px;
-    @include transition(all 0.3s ease-in-out);
-  }
+}
+
+.onoffswitch-checkbox {
+  display: none;
+}
+
+.onoffswitch-checkbox+.onoffswitch-label {
+  border: 1px solid #e1e4ec;
+  -webkit-transition: border 0.3s ease-in-out;
+  transition: border 0.3s ease-in-out;
+}
+
+.onoffswitch-checkbox:checked+.onoffswitch-label {
+  border: 1px solid #6ae2a2;
+  -webkit-transition: border 0.3s ease-in-out;
+  transition: border 0.3s ease-in-out;
+}
+
+.onoffswitch-checkbox:checked+.onoffswitch-label .onoffswitch-inner {
+  margin-left: 0;
+}
+
+.onoffswitch-checkbox:checked+.onoffswitch-label .onoffswitch-switch {
+  right: 2px;
+}
+
+.onoffswitch-checkbox[disabled]+.onoffswitch-label {
+  border: 1px solid #e1e4ec;
+  cursor: not-allowed;
+}
+
+.onoffswitch-checkbox[disabled]+.onoffswitch-label .onoffswitch-inner:before {
+  background-color: #a3a6b2;
+}
+
+.onoffswitch-label {
+  display: block;
+  overflow: hidden;
+  cursor: pointer;
+  margin-bottom: 0;
+  border: 1px solid #6ae2a2;
+  border-radius: 100px;
+}
+
+.onoffswitch-inner {
+  display: block;
+  width: 200%;
+  margin-left: -100%;
+  -webkit-transition: all 0.3s ease-in-out;
+  transition: all 0.3s ease-in-out;
+}
+
+.onoffswitch-inner:before {
+  content: " ";
+  background-color: #6ae2a2;
+}
+
+.onoffswitch-inner:after {
+  content: " ";
+  background-color: #e1e4ec;
+}
+
+.onoffswitch-switch {
+  display: block;
+  width: 20px;
+  height: 20px;
+  margin: 2px 0 0;
+  background: #fff;
+  border-radius: 100%;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 28px;
+  -webkit-transition: all 0.3s ease-in-out;
+  transition: all 0.3s ease-in-out;
 }
 </style>
 <template>
@@ -105,6 +111,7 @@ $lgray:darken(#f1f2f6, 5%);
   </div>
 </template>
 <script>
+import Vue from 'vue';
 export default Vue.extend( {
   data() {
     return {
@@ -122,7 +129,7 @@ export default Vue.extend( {
   },
   computed: {
     randomId() {
-      return 'onOff-' + ( Math.random() * 1000000 ).toFixed()
+      return 'onOff-' + ( Math.random() * 1e9 ).toFixed()
     }
   },
   watch: {
